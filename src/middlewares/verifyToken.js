f365e48b0d33b5c7c84088b45e3c481bb6859f5e
siva <ssankar6281@gmail.com>
2026-01-20 16:28:33 +0530

    Initial commit

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware to verify JWT token
+const verifyToken = (req, res, next) => {
+  let token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access denied. Token is required.' });
+  }else{
+    token=token.split(' ')[1];
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = verifyToken;
